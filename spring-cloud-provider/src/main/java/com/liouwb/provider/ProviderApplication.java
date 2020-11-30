3ed74aeb8cfadc0dd17e9a9cac61a6b23ce2cc3a
liubin <liubin@cardvalue.cn>
2020-11-30 21:39:35 +0800

    1.添加provider模块 2.添加consumer模块 3.添加eureka客户端

---
+++ spring-cloud-provider/src/main/java/com/liouwb/provider/ProviderApplication.java
@@ -0,0 +1,16 @@
+package com.liouwb.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author liouwb
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
