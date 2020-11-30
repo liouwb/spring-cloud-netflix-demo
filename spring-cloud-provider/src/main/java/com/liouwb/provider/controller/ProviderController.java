c792915ff45840760cd3432ec26d15ea904d6c80
liubin <liubin@cardvalue.cn>
2020-11-30 22:24:21 +0800

    1.provider 添加对hystrix的支持

--- spring-cloud-provider/src/main/java/com/liouwb/provider/controller/ProviderController.java
+++ spring-cloud-provider/src/main/java/com/liouwb/provider/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package com.liouwb.provider.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,28 @@ public class ProviderController {
     private String serverPort;
 
     @GetMapping(value = "get")
+    @HystrixCommand(fallbackMethod = "hystrixCall")
     public String get() {
+        double num = Math.random() * 10;
+        if (num > 5) {
+            throw new RuntimeException("provider出现异常");
+        }
+        return "这是一个provider服务,服务端口:" + serverPort;
+    }
+
+    @GetMapping(value = "hystrix_test")
+    @HystrixCommand(fallbackMethod = "hystrixCall")
+    public String hystrixTest() {
+        double num = Math.random() * 10;
+        if (num > 5) {
+            throw new RuntimeException("provider出现异常");
+        }
 
         return "这是一个provider服务,服务端口:" + serverPort;
+    }
+
+    public String hystrixCall(){
+
+        return "@Hystrix:"+serverPort;
     }
 }
